15fb534d6c8d228f7c80b413a53c338cabd78799
Nikoly Cover <nikoly.pereira@pti.org.br>
2024-10-15 17:10:06 -0300

    first commit

---
+++ src/main/java/unioeste/br/contractor_api/ContractorApiApplication.java
@@ -0,0 +1,13 @@
+package unioeste.br.contractor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unioeste/br/contractor_api/ContractorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package unioeste.br.contractor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
